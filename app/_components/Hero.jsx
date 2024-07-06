528266a51a5cd6d828c52ffb66fd6f321954592f
shihhuaien <timothyshih12032007@gmail.com>
2024-07-06 18:04:42 +0800

    feat(ui): update BOOK NOW link to direct to the search page
    Changed the BOOK NOW link to direct to the 'search/所有方案' page to improve navigation.

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -28,7 +28,7 @@ function Hero() {
                             <p className="mt-4 text-gray-600">
                                 想要素顏就美麗，自然健康的睫毛不可少。LUNA Eyelash主打兼顧舒適、自然、眼線感的美睫風格，讓女孩們無論素顏或上妝，都輕鬆擁有清亮眼神。
                             </p>
-                            <Link href='details/1'>
+                            <Link href='search/所有方案'>
                                 <Button className='mt-10 sm:text-xl'>
                                     BOOK NOW
                                 </Button>
